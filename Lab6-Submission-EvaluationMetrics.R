ccb7a5368f0fab847e2397d59e40e8fd58e56e5c
mbuvimuia <innocent.muia@strathmore.edu>
2023-10-23 22:09:18 +0300

    Dataset added

---
+++ Lab6-Submission-EvaluationMetrics.R
@@ -0,0 +1,46 @@
+# STEP 1. Install and Load the Required Packages ----
+## ggplot2 ----
+if (require("ggplot2")) {
+  require("ggplot2")
+} else {
+  install.packages("ggplot2", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## caret ----
+if (require("caret")) {
+  require("caret")
+} else {
+  install.packages("caret", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## mlbench ----
+if (require("mlbench")) {
+  require("mlbench")
+} else {
+  install.packages("mlbench", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## pROC ----
+if (require("pROC")) {
+  require("pROC")
+} else {
+  install.packages("pROC", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## dplyr ----
+if (require("dplyr")) {
+  require("dplyr")
+} else {
+  install.packages("dplyr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+# 1. Accuracy and Cohen's Kappa ----
+## 1.a. Load the dataset ----
+
+
+## 1.b. Determine the Baseline Accuracy ----
